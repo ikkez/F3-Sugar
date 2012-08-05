d71cc1e3b653a3fc6b52b0e3ecc583b17ebaa62a
ikkez <ikkez0n3@gmail.com>
2012-08-05 23:12:28 +0200

    VDB: added testing suite

---
+++ VDB/vdb_tests.php
@@ -0,0 +1,73 @@
+<?php
+
+class VDB_Tests extends F3instance {
+
+	function run() {
+        $this->set('title','Variable DB');
+
+
+        $dbs = array(
+            'mysql' => new VDB(
+                'mysql:host=localhost;port=3306;dbname=test',
+                'root',
+                ''
+            ),
+            'sqlite' => new VDB('sqlite::memory:')
+        );
+
+        foreach( $dbs as $type => $db) {
+
+            $this->set('DB',$db);
+            $type .= ': ';
+
+            // create table
+            $cr_result = $db->createTable('test');
+            $result = $db->getTables();
+            $this->expect(
+                $cr_result == true && in_array('test',$result) == true,
+                $type.'create table',
+                $type.'could not create table'
+            );
+
+            // add column
+            $result1 = $db->addCol('test','title','TEXT8');
+            $this->expect(
+                $result1 == true && in_array('title',$db->getCols('test')) == true,
+                $type.'adding column',
+                $type.'cannot add a column'
+            );
+
+            // remove column
+            $result1 = $db->removeCol('test','title');
+            $this->expect(
+                $result1 == true && in_array('title',$db->getCols('test')) == false,
+                $type.'removng column',
+                $type.'cannot remove a column'
+            );
+
+            // rename table
+            $db->dropTable('test123');
+            $valid = $db->renameTable('test','test123');
+            $this->expect(
+                $valid == true && in_array('test123',$db->getTables()) == true && in_array('test',$db->getTables()) == false,
+                $type.'renaming table',
+                $type.'cannot rename a table'
+            );
+            $db->renameTable('test123','test');
+
+            // drop table
+            $db->dropTable('test');
+            $this->expect(
+                in_array('test',$db->getTables()) == false,
+                $type.'drop table',
+                $type.'cannot not drop table'
+            );
+
+        }
+
+        echo $this->render('basic/results.htm');
+
+	}
+
+
+}
