e9bf5e951fd581b54195c5f892c8aed867bab24e
ikkez <ikkez0n3@gmail.com>
2012-10-11 19:43:34 +0200

    VDB, work in progress: default fields and compositve primary keys, some bug fixes

--- VDB/vdb.php
+++ VDB/vdb.php
@@ -154,7 +154,7 @@ class VDB extends DB {
                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT )"),
             'mysql'=>array(
                 "CREATE TABLE $table (
-                    id SERIAL
+                    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT
                 ) DEFAULT CHARSET=utf8"),
             'pgsql'=>array(
                 "CREATE TABLE $table (id SERIAL PRIMARY KEY)"),
@@ -170,6 +170,82 @@ class VDB extends DB {
         if(!$query[0]) return false;
         return (!$this->exec($query[0]))?TRUE:FALSE;
     }
+
+
+    /**
+     * set primary keys
+     * @param $pks array
+     * @return bool
+     */
+    public function setPKs($pks) {
+        $pk_string = implode(', ',$pks);
+
+        if(preg_match('/sqlite2?/',$this->backend)) {
+            // collect primary key field information
+            $colTypes = $this->getCols(true);
+            $pk_def = array();
+            foreach($pks as $name)
+                $pk_def[] = $name.' '.$colTypes[$name]['type'];
+            $pk_def = implode(', ',$pk_def);
+            // fetch all non primary key fields
+            $newCols = array();
+            foreach ( $colTypes as $colname => $conf)
+                if(!in_array($colname,$pks)) $newCols[$colname] = $conf;
+
+            $this->begin();
+            $oname = $this->name;
+            // rename to temp
+            $this->table($this->name, function($table){
+                $table->renameTable($table->name.'_temp');
+            });
+            // create new origin table, with new private keys and their fields
+            $this->exec("CREATE TABLE $oname ( $pk_def, PRIMARY KEY ($pk_string) );");
+            // create insert trigger to work-a-round autoincrement in multiple primary keys
+            // is set on first PK if it's an int field
+            if(strstr(strtolower($colTypes[$pks[0]]['type']),'int'))
+                $this->exec('CREATE TRIGGER '.$oname.'_insert AFTER INSERT ON '.$oname.
+                            ' WHEN (NEW.'.$pks[0].' IS NULL) BEGIN'.
+                                ' UPDATE '.$oname.' SET '.$pks[0].' = ('.
+                                    ' select coalesce( max( '.$pks[0].' ), 0 ) + 1 from '.$oname.
+                                ') WHERE ROWID = NEW.ROWID;'.
+                            ' END;');
+            // add non-pk fields and import all data
+            $this->table($oname,function($table) use($newCols,$oname) {
+                foreach($newCols as $name => $col)
+                    $table->addCol($name,$col['type'],$col['null'],false,true); //TODO: defaults
+                $fields = implode(', ',$table->getCols());
+                $table->exec('INSERT INTO '.$table->name.'('.$fields.') SELECT '.$fields.' FROM '.$table->name.'_temp');
+            });
+            // drop old table
+            $this->dropTable($oname.'_temp');
+            $this->commit();
+            return true;
+
+        } else {
+            $cmd=array(
+                'sybase|dblib|odbc'=>array(
+                    "CREATE INDEX ".$this->name."_pkey ON ".$this->name." ( $pk_string );"
+                ),
+                'mysql'=>array(
+                    "ALTER TABLE $this->name DROP PRIMARY KEY, ADD PRIMARY KEY ( $pk_string );"),
+                'pgsql'=>array(
+                    "ALTER TABLE $this->name DROP CONSTRAINT ".$this->name.'_pkey;',
+                    "ALTER TABLE $this->name ADD CONSTRAINT ".$this->name."_pkey PRIMARY KEY ( $pk_string );",
+                    ),
+                'mssql'=>array(
+                    ""),
+                'ibm'=>array(
+                    ""),
+            );
+            $query = $this->findQuery($cmd);
+            foreach($query as $q) {
+                if(!$q) return false;
+                $this->exec($q);
+            }
+            return TRUE;
+        }
+    }
+
 
     /**
      * rename a table
@@ -229,7 +305,7 @@ class VDB extends DB {
                     'type'=>$cols[$schema['type']],
                     'null'=>($cols[$schema['nullname']] == $schema['nullval'])?true:false,
                     //'default'=>$cols['Default'], // TODO: add defaults, sqlite: dflt_value
-                    //'extra'=>$cols['Extra'], // TODO: add auto increment
+                    //'extra'=>$cols['Extra'], // TODO: detect auto increment, etc. ?
                 );
             } else
                 $columns[] = $cols[$schema['field']];
@@ -239,13 +315,14 @@ class VDB extends DB {
 
     /**
      * add a column to a table
-     * @param $column
-     * @param $type
-     * @param bool $nullable
+     * @param $column name
+     * @param $type datatype definition
+     * @param bool $nullable allow NULL values
+     * @param $default default insert value
      * @param bool $passThrough
      * @return bool
      */
-    public function addCol($column,$type,$nullable = true,$passThrough = false) {
+    public function addCol($column,$type,$nullable = true,$default = false,$passThrough = false) {
         // check if column is already existing
         if(in_array($column,$this->getCols())) return false;
         //prepare columntypes
@@ -257,14 +334,26 @@ class VDB extends DB {
             $type_val = $this->findQuery($this->dataTypes[strtoupper($type)]);
             if(!$type_val) return false;
         } else $type_val = $type;
-        $null_cmd = ($nullable)?'NULL':'NOT NULL';
+        $null_cmd = ($nullable) ? 'NULL' : 'NOT NULL';
+        $def_cmds = array(
+            'sqlite2?|mysql|pgsql|mssql|sybase|dblib|odbc'=>'DEFAULT',
+            'ibm'=>'WITH DEFAULT',
+        );
+        // not nullable fields should have a default value,
+        // so add predefined default if no other is provided
+        if($default === false && $nullable === false)
+            $def_cmd = $this->findQuery($def_cmds).' '.
+                ((strstr(strtolower($type_val),'int')) ? 0 : "''");
+        else
+            $def_cmd = ($default !== false) ? $this->findQuery($def_cmds).' '.
+                ((is_numeric($default)) ? $default : ("'".$default."'")) : '';
         $cmd=array(
             'sqlite2?'=>array(
-                "ALTER TABLE `$this->name` ADD `$column` $type_val $null_cmd"),
+                "ALTER TABLE `$this->name` ADD `$column` $type_val $null_cmd $def_cmd"),
             'mysql|pgsql|mssql|sybase|dblib|odbc'=>array(
-                "ALTER TABLE $this->name ADD $column $type_val $null_cmd"),
+                "ALTER TABLE $this->name ADD $column $type_val $null_cmd $def_cmd"),
             'ibm'=>array(
-                "ALTER TABLE $this->name ADD COLUMN $column $type_val $null_cmd"),
+                "ALTER TABLE $this->name ADD COLUMN $column $type_val $null_cmd $def_cmd"),
         );
         $query = $this->findQuery($cmd);
         if(!$query[0]) return false;
@@ -289,8 +378,8 @@ class VDB extends DB {
             $this->create($this->name.'_new',function($table) use($newCols) {
                 // TODO: add PK fields
                 foreach($newCols as $name => $col)
-                    $table->addCol($name,$col['type'],$col['null'],true);
-                $fields = (!empty($newCols))?', '.implode(', ',array_keys($newCols)):'';
+                    $table->addCol($name,$col['type'],$col['null'],false,true); // TODO: add default values
+                $fields = (!empty($newCols)) ? ', '.implode(', ',array_keys($newCols)) : '';
                 $table->exec('INSERT INTO '.$table->name.
                     ' SELECT id'.$fields.' FROM '.$table->name);
             });
@@ -334,10 +423,10 @@ class VDB extends DB {
             $oname = $this->name;
             $this->create($this->name.'_new',function($table) use($newCols,$oname) {
                 foreach($newCols as $name => $col)
-                    $table->addCol($name,$col['type'],$col['null'],true);
+                    $table->addCol($name,$col['type'],$col['null'],false,true); // TODO: defaults
                 // TODO: add PK fields here
-                $new_fields = (!empty($newCols))?', '.implode(', ',array_keys($newCols)):'';
-                $cur_fields = implode(', ',array_keys($table->getCols()));
+                $new_fields = (!empty($newCols)) ? ', '.implode(', ',array_keys($newCols)) : '';
+                $cur_fields = implode(', ',$table->getCols());
                 $table->exec('INSERT INTO '.$table->name.'(id'.$new_fields.') SELECT '.$cur_fields.' FROM '.$oname);
             });
             $tname = $this->name;
@@ -348,9 +437,11 @@ class VDB extends DB {
             return true;
         } elseif(preg_match('/odbc/',$this->backend)) {
             // no rename column for odbc
-            $colTypes = $this->getCols(true);
             $this->begin();
-            $this->addCol($column,$colTypes[$column]['type'],$colTypes[$column]['null'],true);
+            $this->table($this->name,function($table) use($column){
+                $colTypes = $table->getCols(true);
+                $table->addCol($column.'_new',$colTypes[$column]['type'],$colTypes[$column]['null'],false,true); // TODO: defaults
+            });
             $this->exec("UPDATE $this->name SET $column_new = $column");
             $this->dropCol($column);
             $this->commit();
