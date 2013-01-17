d181ed67eb2289c4efca030fd1d0a2d392766d28
Christian Knuth <c.knuth@Christian-Knuth-2.local>
2013-01-17 14:29:00 +0100

    porting Pagination Plugin to v3

---
+++ Pagination/README.md
@@ -0,0 +1,129 @@
+## F3 Pagination
+
+create a quick and easy pagination with pagebrowser for your F3 application.
+
+***
+
+### Usage
+
+1.	Copy pagination.php into your lib/ folder OR put the file into one of your include paths, you defined in *AUTOLOAD* ( like `F3::set('AUTOLOAD', 'app/;app/classes');` ).
+
+2.	Pagination uses a *template file* to generate the pagebrowser. Put this template in your UI folder, you've defined like `F3::set('UI','templates/')`
+
+3.	To make the *routing* work, you have to add a new route, containing a token at the end, that could be used by Pagination. 
+	
+	e.g. if you use a route like `F3::route('GET /list', 'Blog->listAllArticles');` 
+	you have to add this route `F3::route('GET /list/@page', 'Blog->listAllArticles');`
+
+
+Within your Controller you can use the following basic code to let the magic begin ;)
+
+´´´ php
+$f3 = \Base::instance();
+
+// set maximus items to be shown per page
+$items_per_page = 20;
+
+// load your itemset and count it all
+$article = new \DB\SQL\Mapper($f3->get('DB'),'article');
+$article_count = $article->count();
+
+// build page links
+$pages = new Pagination($article_count, $items_per_page);
+
+// for template usage, serve generated pagebrowser to the hive
+$f3->set('pagebrowser', $pages->serve()); 
+// use {{ @pagebrowser | raw }} in your template
+
+// get all items for current page
+$articleList = $article->afind(
+	NULL, array(
+		'sort' => 'headline asc', 
+		'limit' => $items_per_page, 
+		'offset' => $pages->getItemOffset()
+	));
+
+// do whatever you like with your fetched data
+// and serve it to template
+
+$f3->set('article',$articleList );
+```
+	
+Is that easy?!
+
+### Configuration
+
+Of course you can define another token key in your route, instead of `@page`. Therefor just set it as third argument on instantiation (here without @-symbol).
+``` php	
+$pages = new Pagination($article_count, $items_per_page, 'paginationToken');
+```	
+If your template is within another subdirectory, or you want to use different templates, you can change the template path with:
+``` php	
+	$pages->setTemplate('templates/pagebrowser.html');
+```
+The Paginator builds links, depending on the current route. But sometimes you maybe want to serve other pagebrowser link. You can set another link path like this:
+``` php	
+$pages->setLinkPath('search/results/');
+```	
+It will now build URLs like `search/results/1`, `search/results/2`, `search/results/3`.
+
+
+### Using <pagination> TAG in Templates
+
+Since F3 v2.0.13 it is possible to setup your own template tags. You can use that shortcut for the Pagination Plugin too. Just register the plugin in the responsible controller like this:
+``` php	
+Template::extend('pagebrowser','Pagination::renderTag');
+```
+	
+You can now use `<pagination items="{{@maxItems}}"></pagination>` to generate the pagination directly form the inside of your template file.
+There are also some more options you can pass through the arguments of that tag to the plugin. A fully configured tag could look like this:
+`<pagebrowser items="253" limit="15" range="5" src="tmpl/foo/bar/pagination.html" token="articlePage"></pagebrowser>`
+
+Of course your can pass variables to all of those arguments, like `range="{{@range}}"`, too.	
+
+
+### Styling
+
+With a bit of basic CSS styling, the output could look like this:
+
+![pagebrowser](http://img7.imagebanana.com/img/4j8o59n4/pagebrowser.png)
+
+``` css
+div.paginator {
+	overflow: hidden;
+	margin-bottom: 10px;
+}
+	div.paginator a {
+		display: block;
+		width: 30px;
+		height: 20px;
+		line-height: 20px;
+		margin: 2px;
+		border: 1px solid #aaa;
+		float: left;
+		text-align: center;
+		-webkit-border-radius: 4px; 
+		-moz-border-radius: 4px; 
+		border-radius: 4px;  
+		font-size: 0.8em;
+	}			
+		div.paginator a:hover {
+			background: #821109;
+			color: #fff !important;
+			text-decoration: none;
+		}
+		
+	div.paginator a.active {
+		background: #ddd;
+	}
+		div.paginator a.active:hover {
+			color: #821109 !important;			
+		}
+```
+
+### some more tuning
+
+-	`setRange($range)`
+	based on your current page, it will display the next and previous pages within `$range`. Default is 2. 
+
+
---
+++ Pagination/pagination.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+    Pagination class for the PHP Fat-Free Framework
+
+    The contents of this file are subject to the terms of the GNU General
+    Public License Version 3.0. You may not use this file except in
+    compliance with the license. Any of the license terms and conditions
+    can be waived if you get permission from the copyright holder.
+
+    Copyright (c) 2012 by ikkez
+    Christian Knuth <mail@ikkez.de>
+
+    @version 1.3.2
+ **/
+
+class Pagination {
+
+    private $items_count;
+    private $items_per_page;
+    private $range = 2;
+    private $current_page;
+    private $template = 'pagebrowser.html';
+    private $routeKey;
+    private $linkPath;
+	private $fw;
+
+    const
+        TEXT_MissingItemsAttr='You need to specify items attribute for a pagination.';
+    /**
+     * create new pagination
+     * @param $items array|integer max items or array to count
+     * @param $limit int max items per page
+     * @param $routeKey string the key for pagination in your routing
+     */
+    public function __construct( $items, $limit = 10, $routeKey = 'page' ) {
+	    $this->fw = \Base::instance();
+        $this->items_count = is_array($items)?count($items):$items;
+        $this->routeKey = $routeKey;
+        $this->setLimit($limit);
+        $this->setCurrent( $this->fw->exists('PARAMS.'.$routeKey) ? $this->fw->get('PARAMS.'.$routeKey) : 1);
+    }
+
+    /**
+     * set maximum items shown on one page
+     * @param $limit int
+     */
+    public function setLimit($limit) {
+        if(is_numeric($limit)) $this->items_per_page = $limit;
+    }
+
+    /**
+     * set path for the template file
+     * @param $template string
+     */
+    public function setTemplate($template) {
+        $this->template = $template;
+    }
+
+    /**
+     * set the range of pages, that are displayed prev and next to current page
+     * @param $range int
+     */
+    public function setRange($range) {
+        if(is_numeric($range)) $this->range = $range;
+    }
+
+    /**
+     * set the current page number
+     * @param $current int
+     */
+    public function setCurrent($current) {
+        if(!is_numeric($current)) return;
+        if($current <= $this->getMax()) $this->current_page = $current;
+        else $this->current_page = $this->getMax();
+    }
+
+    /**
+     * set path to current routing for link building
+     * @param $linkPath
+     */
+    public function setLinkPath($linkPath) {
+        $this->linkPath = (substr($linkPath,0,1) != '/') ? '/'.$linkPath:$linkPath;
+        if(substr($this->linkPath,-1) != '/') $this->linkPath .= '/';
+    }
+
+    /**
+     * returns the current page number
+     * @return int
+     */
+    public function getCurrent() {
+        return $this->current_page;
+    }
+
+    /**
+     * returns the maximum count of items to display in pages
+     * @return int
+     */
+    public function getItemCount() {
+        return $this->items_count;
+    }
+
+    /**
+     * get maximum pages needed to display all items
+     * @return int
+     */
+    public function getMax() {
+        return ceil($this->items_count / $this->items_per_page);
+    }
+
+    /**
+     * get next page number
+     * @return int|bool
+     */
+    public function getNext() {
+        $nextPage = $this->current_page + 1;
+        if( $nextPage > $this->getMax() ) return false;
+        return $nextPage;
+    }
+
+    /**
+     * get previous page number
+     * @return int|bool
+     */
+    public function getPrev() {
+        $prevPage = $this->current_page - 1;
+        if( $prevPage < 1 ) return false;
+        return $prevPage;
+    }
+
+    /**
+     * return last page number, if current page is not in range
+     * @return bool|int
+     */
+    public function getLast() {
+        return ($this->current_page < $this->getMax() - $this->range ) ? $this->getMax() : false;
+    }
+
+    /**
+     * return first page number, if current page is not in range
+     * @return bool|int
+     */
+    public function getFirst() {
+        return ($this->current_page > 3) ? 1 : false;
+    }
+
+    /**
+     * return all page numbers within the given range
+     * @param $range int
+     * @return array page numbers in range
+     */
+    public function getInRange($range = null) {
+        if(is_null($range)) $range = $this->range;
+        $current_range = array( ($this->current_page-$range < 1 ? 1 : $this->current_page-$range),
+            ($this->current_page+$range > $this->getMax() ? $this->getMax() : $this->current_page+$range));
+        $rangeIDs = array();
+        for($x = $current_range[0]; $x <= $current_range[1]; ++$x) {
+            $rangeIDs[] = $x;
+        }
+        return $rangeIDs;
+    }
+
+    /**
+     * returns the number of items left behind for current page
+     * @return int
+     */
+    public function getItemOffset() {
+        return ($this->current_page - 1) * $this->items_per_page;
+    }
+
+    /**
+     * generates the pagination output
+     * @return string
+     */
+    public function serve() {
+        if(is_null($this->linkPath)) {
+            $route = $this->fw->get('PARAMS.0');
+            if($this->fw->exists('PARAMS.'.$this->routeKey)) {
+                $route = str_replace($this->fw->get('PARAMS.'.$this->routeKey),'',$route);
+            } else if(substr($route,-1) != '/') { $route.= '/'; }
+        } else $route = $this->linkPath;
+        $this->fw->set('pg.route',$route);
+        $this->fw->set('pg.currentPage',$this->current_page);
+        $this->fw->set('pg.nextPage',$this->getNext());
+        $this->fw->set('pg.prevPage',$this->getPrev());
+        $this->fw->set('pg.firstPage',$this->getFirst());
+        $this->fw->set('pg.lastPage',$this->getLast());
+        $this->fw->set('pg.rangePages',$this->getInRange());
+        $output = \Template::instance()->render($this->template);
+        $this->fw->clear('pg');
+        return $output;
+    }
+
+    /**
+     * magic render function for custom tags
+     * @static
+     * @param $args
+     * @return string
+     */
+    static public function renderTag($args){
+        var_dump($args);
+
+        $attr = $args['@attrib'];
+        if(!array_key_exists('items',$attr))
+            trigger_error(TEXT_MissingItemsAttr);
+        $f3 = Base::instance();
+        $itemval = $f3->get($attr['items']); // TODO: does not work
+//        $itemval = self::resolve($attr['items']);
+        $items = is_array($itemval)?count($itemval):$itemval;
+        $pn = new self($items);
+        if(array_key_exists('limit',$attr))
+            $pn->setLimit($f3->get($attr['limit']));
+        if(array_key_exists('range',$attr))
+            $pn->setRange($f3->get($attr['range']));
+        if(array_key_exists('src',$attr))
+            $pn->setTemplate($f3->get($attr['src']));
+        if(array_key_exists('token',$attr))
+            $pn->routeKey($f3->get($attr['token']));
+
+        return $pn->serve();
+    }
+}
