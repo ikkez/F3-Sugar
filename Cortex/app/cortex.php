a30c9458a26509f0443a3d4e59728e06f14b2962
ikkez <ikkez0n3@gmail.com>
2013-02-04 08:27:42 +0100

    adding some tests

--- Cortex/app/cortex.php
+++ Cortex/app/cortex.php
@@ -4,137 +4,128 @@ namespace App;
 
 class Cortex extends Controller
 {
-	function get()
-	{
-		$f3 = \Base::instance();
-		$test = new \Test;
+    function get()
+    {
+        $f3 = \Base::instance();
+        $test = new \Test;
 
-		$f3->set('QUIET', false);
+        $f3->set('QUIET', false);
 
-		$dbs = array(
-			'sql' => new \DB\SQL('mysql:host=localhost;port=3306;dbname=fatfree', 'fatfree', ''	),
-			'jig' => new \DB\Jig('data/'),
-			'mongo' => new \DB\Mongo('mongodb://localhost:27017', 'testdb')
-		);
+        $dbs = array(
+            'sql' => new \DB\SQL('mysql:host=localhost;port=3306;dbname=fatfree', 'fatfree', ''),
+            'jig' => new \DB\Jig('data/'),
+            'mongo' => new \DB\Mongo('mongodb://localhost:27017', 'testdb')
+        );
 
-		$tname = 'test_cortex';
+        $tname = 'test_cortex';
 
-		foreach ($dbs as $type => $db) {
+        foreach ($dbs as $type => $db) {
 
-			\DB\Cortex::setdown($db, $tname);
-			
-			$fields = array(
-				'title'=>array('type'=>\DB\SQL\Schema::DT_TEXT8),
-				'num1'=>array('type'=> \DB\SQL\Schema::DT_INT8),
-				'num2'=>array('type'=> \DB\SQL\Schema::DT_INT8),
-			);
-			\DB\Cortex::setup($db, $tname,$fields);
-			
-			// adding some testing data
-			$cx = new \DB\Cortex($db, $tname);
-			$cx->title = 'bar1';
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'baz2';
-	        $cx->num1 = 1;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo3';
-	        $cx->num1 = 4;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo4';
-	        $cx->num1 = 3;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo5';
-	        $cx->num1 = 3;
-	        $cx->num2 = 5;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo6';
-	        $cx->num1 = 3;
-	        $cx->num2 = 1;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo7';
-	        $cx->num1 = 3;
-	        $cx->num2 = 10;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo8';
-	        $cx->num1 = 5;
-	        $cx->save();
-	        $cx->reset();
-	
-	        $cx->title = 'foo9';
-	        $cx->num1 = 8;
-	        $cx->save();
-	        $cx->reset();
-	        
-	        $result = $this->getResult($cx->find());
+            \DB\Cortex::setdown($db, $tname);
 
-			$expected = array (
-							  0 =>
-								  array (
-								    'title' => 'bar1',
-								  ),
-							  1 =>
-								  array (
-								    'title' => 'baz2',
-								    'num1' => 1,
-								  ),
-							  2 =>
-								  array (
-								    'title' => 'foo3',
-								    'num1' => 4,
-								  ),
-							  3 =>
-								  array (
-								    'title' => 'foo4',
-								    'num1' => 3,
-								  ),
-							  4 =>
-								  array (
-								    'title' => 'foo5',
-								    'num1' => 3,
-								    'num2' => 5,
-								  ),
-							  5 =>
-								  array (
-								    'title' => 'foo6',
-								    'num1' => 3,
-								    'num2' => 1,
-								  ),
-							  6 =>
-								  array (
-								    'title' => 'foo7',
-								    'num1' => 3,
-								    'num2' => 10,
-								  ),
-							  7 =>
-								  array (
-								    'title' => 'foo8',
-								    'num1' => 5,
-								  ),
-							  8 =>
-								  array (
-								    'title' => 'foo9',
-								    'num1' => 8,
-								  ),
-							);
+            $fields = array(
+                'title' => array('type' => \DB\SQL\Schema::DT_TEXT8),
+                'num1' => array('type' => \DB\SQL\Schema::DT_INT8),
+                'num2' => array('type' => \DB\SQL\Schema::DT_INT8),
+            );
+            \DB\Cortex::setup($db, $tname, $fields);
 
-			$test->expect(
-				json_encode($result) == json_encode($expected),
-				$type.': init mapper, adding records'
-			);
+            // adding some testing data
+            $cx = new \DB\Cortex($db, $tname);
+            $cx->title = 'bar1';
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'baz2';
+            $cx->num1 = 1;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo3';
+            $cx->num1 = 4;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo4';
+            $cx->num1 = 3;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo5';
+            $cx->num1 = 3;
+            $cx->num2 = 5;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo6';
+            $cx->num1 = 3;
+            $cx->num2 = 1;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo7';
+            $cx->num1 = 3;
+            $cx->num2 = 10;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo8';
+            $cx->num1 = 5;
+            $cx->save();
+            $cx->reset();
+
+            $cx->title = 'foo9';
+            $cx->num1 = 8;
+            $cx->save();
+            $cx->reset();
+
+            $result = $this->getResult($cx->find());
+
+            $expected = array(
+                0 => array(
+                    'title' => 'bar1',
+                ),
+                1 => array(
+                    'title' => 'baz2',
+                    'num1' => 1,
+                ),
+                2 => array(
+                    'title' => 'foo3',
+                    'num1' => 4,
+                ),
+                3 => array(
+                    'title' => 'foo4',
+                    'num1' => 3,
+                ),
+                4 => array(
+                    'title' => 'foo5',
+                    'num1' => 3,
+                    'num2' => 5,
+                ),
+                5 => array(
+                    'title' => 'foo6',
+                    'num1' => 3,
+                    'num2' => 1,
+                ),
+                6 => array(
+                    'title' => 'foo7',
+                    'num1' => 3,
+                    'num2' => 10,
+                ),
+                7 => array(
+                    'title' => 'foo8',
+                    'num1' => 5,
+                ),
+                8 => array(
+                    'title' => 'foo9',
+                    'num1' => 8,
+                ),
+            );
+
+            $test->expect(
+                json_encode($result) == json_encode($expected),
+                $type.': init mapper, adding records'
+            );
 
             // operator =
             $result = $this->getResult($cx->find(array('title = ?','foo7')));
@@ -174,7 +165,6 @@ class Cortex extends Controller
                 $type.': operator check: >='
             );
 
-
             // operator <
             $result = $this->getResult($cx->find(array('num2 < ?',2)));
             $expected = array(
@@ -195,26 +185,26 @@ class Cortex extends Controller
                 json_encode($result) == json_encode($expected),
                 $type.': operator check: <='
             );
-            
-			// field comparision
-	        $result = $this->getResult($cx->find(
-	            array('num2 > num1',1)));
-	        $expected = array (
-	          	0 => array (
-	            	'title' => 'foo5',
-	            	'num1' => 3,
-	            	'num2' => 5,
-	          	),
-	          	1 => array (
-	            	'title' => 'foo7',
-	            	'num1' => 3,
-	            	'num2' => 10,
-	          	),
-			);
-			$test->expect(
-				json_encode($result) == json_encode($expected),
-				$type.': check field comparision'
-			);
+
+            // field comparision
+            $result = $this->getResult($cx->find(
+                array('num2 > num1', 1)));
+            $expected = array(
+                0 => array(
+                    'title' => 'foo5',
+                    'num1' => 3,
+                    'num2' => 5,
+                ),
+                1 => array(
+                    'title' => 'foo7',
+                    'num1' => 3,
+                    'num2' => 10,
+                ),
+            );
+            $test->expect(
+                json_encode($result) == json_encode($expected),
+                $type.': check field comparision'
+            );
             
             // lookahead search
             $result = $this->getResult($cx->find(array('title like ?','%o6')));
@@ -246,7 +236,7 @@ class Cortex extends Controller
             $result = $this->getResult($cx->find(array('title like ?','%a%')));
             $expected = array(
                 0 => array(
-                    'title' => 'bar1',                                       
+                    'title' => 'bar1',
                 ),
                 1 => array(
                     'title' => 'baz2',
@@ -256,30 +246,72 @@ class Cortex extends Controller
             $test->expect(
                 json_encode($result) == json_encode($expected),
                 $type.': full search'
-            );         
+            );
 
-			// AND / OR chaining
-			$result = $this->getResult($cx->find(
-				array('(num2 < ? AND num1 > ?) OR title like ?', 2, 1, '%o9')));
-			$expected = array(
-				0 => array(
-					'title' => 'foo6',
-					'num1' => 3,
-					'num2' => 1,
-				),
-				1 => array(
-					'title' => 'foo9',
-					'num1' => 8,
-				),
-			);
-			$test->expect(
-				json_encode($result) == json_encode($expected),
-				$type.': check logical operator chaining'
-			);
-			
-		}
-		$f3->set('results', $test->results());
-	}
+            // AND / OR chaining
+            $result = $this->getResult($cx->find(
+                array('(num2 < ? AND num1 > ?) OR title like ?', 2, 1, '%o9')));
+            $expected = array(
+                0 => array(
+                    'title' => 'foo6',
+                    'num1' => 3,
+                    'num2' => 1,
+                ),
+                1 => array(
+                    'title' => 'foo9',
+                    'num1' => 8,
+                ),
+            );
+            $test->expect(
+                json_encode($result) == json_encode($expected),
+                $type.': check logical operator chaining'
+            );
+
+            // check limit
+            $result = $this->getResult($cx->find(
+                null,array('limit'=>'2')));
+            $expected = array(
+                0 => array(
+                    'title' => 'bar1',
+                ),
+                1 => array(
+                    'title' => 'baz2',
+                    'num1' => 1,
+                ),
+            );
+            $test->expect(
+                json_encode($result) == json_encode($expected),
+                $type.': check limit'
+            );
+
+            // check order
+            $result = $this->getResult($cx->find(
+                array('num2 >= ?',1), array('order'=>'num2 desc')));
+            $expected = array(
+                0 => array(
+                    'title' => 'foo7',
+                    'num1' => 3,
+                    'num2' => 10,
+                ),
+                1 => array(
+                    'title' => 'foo5',
+                    'num1' => 3,
+                    'num2' => 5,
+                ),
+                2 => array(
+                    'title' => 'foo6',
+                    'num1' => 3,
+                    'num2' => 1,
+                ),
+            );
+            $test->expect(
+                json_encode($result) == json_encode($expected),
+                $type.': check order'
+            );
+
+        }
+        $f3->set('results', $test->results());
+    }
 
     /**
      * unify results for better comparison
