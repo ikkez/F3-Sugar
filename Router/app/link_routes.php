5dda39be240caf93ea7d01e91782744c0c3c9101
ikkez <ikkez0n3@gmail.com>
2013-11-18 23:16:19 +0100

    update unit tests

--- Router/app/link_routes.php
+++ Router/app/link_routes.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
-class Link_Routes extends Controller {
+class Link_Routes extends Controller
+{
 
-	function get($f3) {
+    function get($f3)
+    {
 
-        if(!$f3->exists('GET.foo'))
+        if (!$f3->exists('GET.foo'))
             $f3->reroute($f3->get('PARAMS.0').'?foo=bar');
 
         $router = \Router::instance();
@@ -25,85 +27,122 @@ class Link_Routes extends Controller {
         $f3->set('rr', 'article-action');
         $f3->set('t1', 'groovy');
         $f3->set('querystring', 'foo=bar&baz=narf');
-        $f3->set('querystringArray', array('foo'=>'bar','baz'=>'narf'));
+        $f3->set('querystringArray', array('foo' => 'bar', 'baz' => 'narf'));
         $f3->set('section', 'anchor-2');
+        $f3->set('class', 'button');
+        $f3->set('param', 'rel="lightbox"');
 
         $template = \Template::instance();
         $test = new \Test;
 
         $result = $template->render('templates/link_routes.html');
-        $lines = explode("\n",$result);
-
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="test1">simple route</a><br/>',
-			'simple route'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="article/view/foo2">route with param</a><br/>',
-			'route with param'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2">simple route</a><br/>',
-			'simple route 2'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="article/view/groovy">vars by token</a><br/>',
-			'resolve route param token'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="article/edit/groovy">multiple params</a><br/>',
-			'multiple params'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="article/view/$blah">inject test</a><br/>',
-			'inject test'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="routing-map">map test</a><br/>',
-			'map test'
-		);
-		$test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string test</a><br/>',
-			'query test'
-		);
+        $lines = explode("\n", $result);
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string from string token</a><br/>',
-			'query string from string token'
-		);
+            trim(array_shift($lines)) == '<a href="test1">simple link</a>',
+            'simple link'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="article/view/foo2">route with param</a>',
+            'route with param'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="routing-2">simple route</a>',
+            'simple route 2'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="article/view/groovy">vars by token</a>',
+            'resolve route param token'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="article/edit/groovy">multiple params</a>',
+            'multiple params'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="article/view/$blah">inject test</a>',
+            'inject test'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="routing-map">map test</a>',
+            'map test'
+        );
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string test</a>',
+            'query test'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string from array token</a><br/>',
-			'query string from array token'
-		);
+            trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string from string token</a>',
+            'query string from string token'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="/routing-2">absolute path</a><br/>',
-			'absolute path'
-		);
+            trim(array_shift($lines)) == '<a href="routing-2?foo=bar&amp;baz=narf">query string from array token</a>',
+            'query string from array token'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="'.$f3->get('SCHEME').'://'.$f3->get('HOST').
-            $f3->get('BASE').'/routing-2">full absolute path with host and base</a><br/>',
-			'full absolute path with host and base'
-		);
+            trim(array_shift($lines)) == '<a href="/routing-2">absolute path</a>',
+            'absolute path'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2?foo=bar">add existing query string to link</a><br/>',
-			'add existing query string to link'
-		);
+            trim(array_shift($lines)) == '<a href="'.$f3->get('SCHEME').'://'.$f3->get('HOST').
+            $f3->get('BASE').'/routing-2">full absolute path with host and base</a>',
+            'full absolute path with host and base'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2#anchor-1">link with anchor section</a><br/>',
-			'link with #anchor section'
-		);
+            trim(array_shift($lines)) == '<a href="routing-2?foo=bar">add existing query string to link</a>',
+            'add existing query string to link'
+        );
 
         $test->expect(
-			trim(array_shift($lines)) == '<a href="routing-2#anchor-2">link anchor from token</a><br/>',
-			'link #anchor from token'
-		);
+            trim(array_shift($lines)) == '<a href="routing-2#anchor-1">link with anchor section</a>',
+            'link with #anchor section'
+        );
 
-		$f3->set('results',$test->results());
-	}
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="routing-2#anchor-2">link anchor from token</a>',
+            'link #anchor from token'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="test1" class="button">1. simple link with token</a>',
+            'simple link with token'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="test2" rel="lightbox">2. link with inline token</a>',
+            'link with inline token'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="test3" rel="lightbox" class="btn">3. link, inline token, params</a>',
+            'link, inline token, params'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a class="button" href="routing-2">4. route with parameter token</a>',
+            'route with parameter token'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a rel="lightbox" href="routing-2">5. route, inline token</a>',
+            'route with inline token'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a rel="lightbox" class="btn" href="routing-2">6. route, inline token, params</a>',
+            'route with inline token and param'
+        );
+
+        $test->expect(
+            trim(array_shift($lines)) == '<a href="test4" disabled>link with value-less parameter</a>',
+            'link with value-less parameter'
+        );
+
+        $f3->set('results', $test->results());
+    }
 
 }
