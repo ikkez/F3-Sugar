276cbf4c03bbf1611455a38fa0915d1304c76713
ikkez <ikkez0n3@gmail.com>
2015-08-07 16:55:41 +0200

    better support for array key names, #48

--- FooForms/lib/template/tags/input.php
+++ FooForms/lib/template/tags/input.php
@@ -10,8 +10,8 @@
  *	Copyright (c) 2015 ~ ikkez
  *	Christian Knuth <ikkez0n3@gmail.com>
  *
- *	@version: 0.2.0
- *	@date: 14.04.2015
+ *	@version: 0.3.0
+ *	@date: 07.08.2015
  *
  **/
 
@@ -37,33 +37,33 @@ class Input extends \Template\TagHandler {
 		$srcKey = \Base::instance()->get('template.form.srcKey');
 		if (isset($attr['type']) && isset($attr['name'])) {
 			$name = $this->tokenExport($attr['name']);
-			if ($attr['type'] == 'checkbox') {
+			$ar_name = preg_replace('/\'*(\w+)(\[.*\])\'*/i','[$1]$2',$name,-1,$i);
+			$name = $i ? $ar_name : '['.$name.']';
+
+			if (($attr['type'] == 'checkbox') ||
+				($attr['type'] == 'radio' && isset($attr['value']))
+			) {
 				$value = $this->tokenExport(isset($attr['value'])?$attr['value']:'on');
-				// basic match
-				$str = '(isset(@'.$srcKey.'['.$name.']) && @'.$srcKey.'['.$name.']=='.$value.')';
-				// dynamic array match
-				if (preg_match('/({{.+?}})/s', $attr['name'])) {
-					$str.= ' || (isset(@'.$srcKey.'[substr('.$name.',0,-2)]) && is_array(@'.$srcKey.'[substr('.$name.',0,-2)])'.
-						' && in_array('.$value.',@'.$srcKey.'[substr('.$name.',0,-2)]))';
-				}
 				// static array match
-				elseif (preg_match('/(\[\])/s', $attr['name'])) {
-					$name=substr($attr['name'],0,-2);
-					$str='(isset(@'.$srcKey.'['.$name.']) && is_array(@'.$srcKey.'['.$name.'])'.
-						' && in_array('.$value.',@'.$srcKey.'['.$name.']))';
+				if (preg_match('/(\[\])/s', $name)) {
+					$name=substr($name,0,-2);
+					$str='(isset(@'.$srcKey.$name.') && is_array(@'.$srcKey.$name.')'.
+						' && in_array('.$value.',@'.$srcKey.$name.'))';
+				} else {
+					// basic match
+					$str = '(isset(@'.$srcKey.$name.') && @'.$srcKey.$name.'=='.$value.')';
+					// dynamic array match
+					if (preg_match('/({{.+?}})/s', $attr['name'])) {
+						$str.= ' || (isset(@'.$srcKey.$name.') && is_array(@'.$srcKey.$name.')'.
+							' && in_array('.$value.',@'.$srcKey.$name.'))';
+					}
 				}
 				$str = '{{'.$str.'?\'checked="checked"\':\'\'}}';
 				$attr[] = $this->tmpl->build($str);
 
-			} elseif ($attr['type'] == 'radio' && isset($attr['value'])) {
-				$value = $this->tokenExport(isset($attr['value'])?$attr['value']:'on');
-				$attr[] = $this->tmpl->build('{{ isset(@'.$srcKey.'['.$name.']) && '.
-					'@'.$srcKey.'['.$attr['name'].']=='.$value.'?\'checked="checked"\':\'\'}}');
 			} elseif($attr['type'] != 'password' && !array_key_exists('value',$attr)) {
 				// all other types, except password fields
-				$ar_name = preg_replace('/\'*(\w+)(\[.*\])\'*/i','[$1]$2',$name,-1,$i);
-				$name = $i ? $ar_name : '['.$name.']';
-				$attr['value'] = $this->tmpl->build('{{ isset(@'.$srcKey.''.$name.')?@'.$srcKey.''.$name.':\'\'}}');
+				$attr['value'] = $this->tmpl->build('{{ isset(@'.$srcKey.$name.')?@'.$srcKey.$name.':\'\'}}');
 			}
 		}
 		// resolve all other / unhandled tag attributes
